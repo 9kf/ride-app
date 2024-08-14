c0830ef101593b193cb94398ea6c285466d32a9e
9kf <kf.9francisco@gmail.com>
2024-08-14 14:00:35 +0800

    fixed bug on ride views and docker-compose setup

--- ride/serializers.py
+++ ride/serializers.py
@@ -1,32 +1,37 @@
 from rest_framework import serializers
 from .models import Ride, RideEvent
 from django.contrib.auth.models import User
+import datetime
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields= ['id', 'username', 'first_name', 'last_name', 'email']
 
-class RideSerializer(serializers.ModelSerializer):
-    rider = serializers.SerializerMethodField()
-    driver = serializers.SerializerMethodField()
-    class Meta:
-        model = Ride
-        fields = ['id', 'status', 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude', 'pickup_time', 'rider', 'driver']
-
-    def get_rider(self, obj):
-        return UserSerializer(obj.rider).data
-    
-    def get_driver(self, obj):
-        return UserSerializer(obj.driver).data
-
 class RideEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = RideEvent
         fields = '__all__'
+
+class RideSerializer(serializers.ModelSerializer):
+    rider_lookup = serializers.SerializerMethodField()
+    driver_lookup = serializers.SerializerMethodField()
+    todays_ride_events = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Ride
+        fields = ['id', 'status', 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude', 'pickup_time', 'rider', 'driver', 'todays_ride_events', 'rider_lookup', 'driver_lookup']
+
+    def get_rider_lookup(self, obj):
+        return UserSerializer(obj.rider).data
     
-    def __str__(self):
-        return str(self.title)
+    def get_driver_lookup(self, obj):
+        return UserSerializer(obj.driver).data
+
+    def get_todays_ride_events(self,obj):
+        ride_events = RideEvent.objects.filter(id_ride=obj.id,created_at__gt=datetime.date.today() - datetime.timedelta(days=1))
+        events_serializer = RideEventSerializer(ride_events, many=True)
+        return events_serializer.data
 
 
     
--- ride/views.py
+++ ride/views.py
@@ -1,40 +1,20 @@
 import datetime
 from rest_framework.response import Response
-from rest_framework.response import Response
 from rest_framework import permissions, viewsets
 from django.contrib.auth.models import User
 from .models import Ride, RideEvent, STATUS_CHOICES
 from .serializers import RideSerializer, RideEventSerializer
+from rest_framework.pagination import PageNumberPagination
 
-class RidesListViewset(viewsets.ViewSet):
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class RidesListViewset(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
     queryset = Ride.objects.all()
-
-    def list(self, request):
-        ride_status = request.query_params.get('status')
-        rider_email = request.query_params.get('rider-email')
-
-        filtered_queryset = self.queryset
-
-        if ride_status is not None:
-            if ride_status in dict(STATUS_CHOICES):
-                filtered_queryset = filtered_queryset.filter(status=ride_status)
-            else:
-                raise Exception("Invalid ride status")
-
-        if rider_email is not None:
-            user = User.objects.filter(email=rider_email).first()
-            if user is not None:
-                filtered_queryset = filtered_queryset.filter(rider=user)
-            else:
-                raise Exception('Invalid user')
-
-        ride_events = RideEvent.objects.filter(id_ride=self.queryset.first().pk, created_at__gt=datetime.date.today() - datetime.timedelta(days=1))
-
-        ride_serialzer = RideSerializer(filtered_queryset, many=True)
-        events_serializer = RideEventSerializer(ride_events, many=True)
-
-        return Response({"rides": ride_serialzer.data, "todays_ride_events": events_serializer.data})
+    serializer_class = RideSerializer
     
 class RideEventViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
